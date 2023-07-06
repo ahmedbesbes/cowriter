507b6079fa6269f8a0ce2896cceab2013510bd1d
Ahmed Besbes <ahmed.besbes@hotmail.com>
2023-07-06 23:52:23 +0200

    Add logger

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+import logging
+import click
+from rich.logging import RichHandler
+
+
+LOGGER_NAME = "cowriter"
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s:%(levelname)s:%(name)s:%(message)s",
+    handlers=[RichHandler(rich_tracebacks=True, tracebacks_suppress=[click])],
+)
+logger = logging.getLogger(LOGGER_NAME)
